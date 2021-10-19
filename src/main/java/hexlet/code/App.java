297789ea4daea01883f0979a5e953fb78ec35114
Sergey Ridev <whereareyo@yahoo.com>
2021-10-20 00:34:17 +0300

    javalin added

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.enableDevLogging();
+        });
+        addRoutes(app);
+        return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.valueOf(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello world!"));
     }
 }
