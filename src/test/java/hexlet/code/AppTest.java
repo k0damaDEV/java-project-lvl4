459c855abf9717afd41e75205b866ff4c98fcc6e
Sergey Ridev <whereareyo@yahoo.com>
2021-10-20 00:13:08 +0300

    jacoco added

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,4 +1,11 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTest {
+    @Test
+    void mainTest() {
+        assertThat(true).isEqualTo(true);
+    }
 }
