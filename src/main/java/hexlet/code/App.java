a1f59b43dcda6fcf5b19d001e5f289cf8200b418
Sergey Ridev <whereareyo@yahoo.com>
2021-10-26 03:44:43 +0300

    fix

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -59,7 +59,7 @@ public class App {
     }
 
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "4000");
+        String port = System.getenv().getOrDefault("PORT", "5000");
         return Integer.valueOf(port);
     }
 
