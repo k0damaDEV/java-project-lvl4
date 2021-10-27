724faff66cfb6547e284f6c83aa41fab139a9ef5
Sergey Ridev <whereareyo@yahoo.com>
2021-10-27 15:33:04 +0300

    lint fix

--- src/main/java/hexlet/code/domain/Url.java
+++ src/main/java/hexlet/code/domain/Url.java
@@ -3,7 +3,10 @@ package hexlet.code.domain;
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Column;
 import java.time.Instant;
 import java.util.List;
 
